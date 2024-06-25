cdee511c787a92c74e5bf30f3b5e195a6ed6f0a4
sezna <alehansen@microsoft.com>
2024-06-24 22:30:18 -0700

    update tests

--- petr-ir/src/lib.rs
+++ petr-ir/src/lib.rs
@@ -490,7 +490,7 @@ mod tests {
                 function 0:
                  0	pop v0
                  1	pop v1
-                 2	push v0
+                 2	push v1
                  3	ret
                 ENTRY: function 1:
                  4	ld v2 datalabel0
@@ -530,7 +530,35 @@ mod tests {
                     a
                 function main() returns 'int ~hi(1, 2)
                 "#,
-            expect![[r#""#]],
+            expect![[r#"
+                ; DATA_SECTION
+                0: Int64(20)
+                1: Int64(30)
+                2: Int64(42)
+                3: Int64(1)
+                4: Int64(2)
+
+                ; PROGRAM_SECTION
+                	ENTRY: 1
+                function 0:
+                 0	pop v0
+                 1	pop v1
+                 2	cp v2 v1
+                 3	cp v3 v0
+                 4	ld v4 datalabel0
+                 5	ld v5 datalabel1
+                 6	ld v6 datalabel2
+                 7	push v2
+                 8	ret
+                ENTRY: function 1:
+                 9	ld v7 datalabel3
+                 10	push v7
+                 11	ld v8 datalabel4
+                 12	push v8
+                 13	ppc
+                 14	jumpi functionid0
+                 15	ret
+            "#]],
         );
     }
 }
