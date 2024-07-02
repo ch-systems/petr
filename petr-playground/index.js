1409753938f89cc0eda5492d9eb93b9ecc9aa4d4
Alex <alex@alex-hansen.com>
2024-07-02 10:27:33 -0700

    add webpack config and set up monaco editor

--- petr-playground/index.js
+++ petr-playground/index.js
@@ -1,8 +1,19 @@
 import { greet, run_snippet } from './pkg';
 
+/*
 greet('World');
 
 let result = run_snippet("function main() returns 'int 42");
 
 
 greet(result.toString());
+*/
+
+import * as monaco from 'monaco-editor';
+// or import * as monaco from 'monaco-editor/esm/vs/editor/editor.api';
+// if shipping only a subset of the features & languages is desired
+
+monaco.editor.create(document.getElementById('monaco-editor'), {
+	value: 'console.log("Hello, world")',
+	language: 'javascript'
+});
